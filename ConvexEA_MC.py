5ef82109ba2a4873bac86cba9bc8596f91103067
imvee <vind.veeru@gmail.com>
2023-01-10 00:29:33 +0530

    Added smoothness and symmetry to fitness function

--- ConvexEA_MC.py
+++ ConvexEA_MC.py
@@ -44,6 +44,37 @@ def crossover(C1,C2):
     
     return c1,c2
 
+def calculate_smoothness(vertices, faces):
+    smoothness = 0
+    
+    for face in faces:
+        
+        v1, v2, v3 = vertices[face]
+        
+        normal = np.cross(v2 - v1, v3 - v1)
+        
+        angle1 = np.arccos(np.dot(normal, v2 - v1) / (np.linalg.norm(normal) * np.linalg.norm(v2 - v1)))
+        angle2 = np.arccos(np.dot(normal, v3 - v2) / (np.linalg.norm(normal) * np.linalg.norm(v3 - v2)))
+        angle3 = np.arccos(np.dot(normal, v1 - v3) / (np.linalg.norm(normal) * np.linalg.norm(v1 - v3)))
+        
+        smoothness += angle1 + angle2 + angle3
+    
+    smoothness /= len(faces)
+    
+    return smoothness
+
+def calculate_symmetry(vertices, faces):
+    # Calculate the center of the mesh
+    center = np.mean(vertices, axis=0)
+
+    # Calculate the distance of each vertex from the center
+    distances = np.linalg.norm(vertices - center, axis=1)
+
+    # Calculate the standard deviation of the distances
+    std_dev = np.std(distances)
+
+    return 1 - std_dev
+
 def golden_ratio(vertices, faces):
       
   total_length = 0
