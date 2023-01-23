f7abaa0f42e32787b7ec8f80e60b9f411debd017
imvee <vind.veeru@gmail.com>
2023-01-24 02:29:55 +0530

    Modified CGen

--- generate.py
+++ generate.py
@@ -9,14 +9,23 @@ FILE_NUM=my_input[4]
 
 VERTS_FILE=my_input[5]
 
-MATERIALS_FILE=my_input[7]
+MATERIALS=literal_eval(my_input[6])
 
-RO=int(my_input[8])
-OUTPUT_PATH=my_input[9]
+BD=my_input[7]
+ME=my_input[8]
+RO=my_input[9]
+SS=my_input[10]
+
+OUTPUT_PATH=my_input[11]
+
+with open(OUTPUT_PATH+"\\"+VERTS_FILE, 'r') as f:
+    VERTS = f.readlines()
+
+VERTS=literal_eval(VERTS[0])
 
 
 # Generate a random set of vertices
-vertices = 
+vertices = VERTS
 
 # Create a new curve object and set the vertices
 curve_data = bpy.data.curves.new('MyCurve', type='CURVE')
@@ -37,7 +46,7 @@ bpy.context.collection.objects.link(curve_obj)
 
 # Extrude the curve to create a skin-like surface
 curve_obj.data.splines[0].use_smooth = True
-curve_obj.data.bevel_depth = 0.75
+curve_obj.data.bevel_depth = BD
 
 bpy.context.view_layer.objects.active = bpy.data.objects[0]
 
@@ -47,12 +56,12 @@ obj = bpy.data.objects["MyCurve"]
 modifier = obj.modifiers.new(name="Subdivision", type='SUBSURF')
 
 #Set the value of the modifier (e.g. the number of subdivisions)
-modifier.levels = 6
+modifier.levels = SS
 
 #material
 mat = bpy.data.materials.new("MyMaterial")
-mat.diffuse_color = COLOUR
-mat.metallic=0.7
-mat.roughness=0.0
+mat.diffuse_color = MATERIALS
+mat.metallic=ME
+mat.roughness=RO
 
 obj.data.materials.append(mat)
